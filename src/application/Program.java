fd8d284a93455c2a4e18326ecede5f781151b928
Nattan <nattan078@gmail.com>
2022-02-03 10:13:07 -0300

    Implementando update

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,6 +34,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Pablo", "pablo@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\nTESTE 5: seller update");
+		seller = sellerDao.findById(1);
+		seller.setName("André Alexandre");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
